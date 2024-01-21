32f38a1d0abeeb831dee21475e0131c1b43208f0
Sintong Maranata Hutasoit <126843082+sintonghtst@users.noreply.github.com>
2024-01-21 18:35:36 +0700

    Add files via upload

--- allhelp/js/96f5c0d4b9bdbf07570efd8e94cb7db7a5191a01/index.js
+++ allhelp/js/96f5c0d4b9bdbf07570efd8e94cb7db7a5191a01/index.js
@@ -2,9 +2,7 @@
 const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
 const smallMenu = document.querySelector('.header__sm-menu')
 const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
-const headerHamMenuCloseBtn = document.querySelector(
-  '.header__main-ham-menu-close'
-)
+const headerHamMenuCloseBtn = document.querySelector('.header__main-ham-menu-close')
 const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
 
 hamMenuBtn.addEventListener('click', () => {
